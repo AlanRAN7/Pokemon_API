524562595775fe419e1e4e11227b1188df376f98
Alan Álvarez <aran.712@gmail.com>
2022-03-02 23:02:44 -0600

    Added: POST Petitions, Ternary Operator, Filter Method, Body Parser

--- index.js
+++ index.js
@@ -11,18 +11,32 @@ const app = express();
 // const pokedex = require("./pokedex.json");
 const {pokemon} = require("./pokedex.json")
 
+/*
+GET: Obtener recursos
+POST: Almacenar / crear recursos
+PATCH: Modificar una parte de un recurso
+PUT: Modificar un recurso
+DELETE: Borrar un recurso
+*/
+//Se usa cuando una función se quiera usar en todas las peticiones que entra en el servidor
+// use = middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //Express trabaja de arriba hacia abajo como jerarquia
 app.get("/", (req, res, next)=>{
     // const pokemon = pokedex.pokemon;
-    res.status(200);
-    // res.send(pokemon);
-    res.send("Bienvenido al Pokedex")
+    return res.status(200).send("Bienvenido al Pokedex")
 
 })
 
-app.get("/pokemon/all", (req,res,next)=>{
-    res.status(200);
-    res.send(pokemon);
+app.post("/pokemon", (req, res, next)=>{
+    return res.status(200).send(req.body)
+})
+
+app.get("/pokemon", (req,res,next)=>{
+    return res.status(200).send(pokemon);
 })
 
 //:id - Variable
@@ -30,26 +44,31 @@ app.get("/pokemon/all", (req,res,next)=>{
 app.get("/pokemon/:id([0-9]{1,3})", (req,res,next)=>{
     const id = req.params.id - 1;
     if(id >= 0 && id<=150){
-        res.status(200);
-        res.send(pokemon[req.params.id - 1])
+        return res.status(200).send(pokemon[req.params.id - 1])
     } else{
-        res.status(404);
-        res.send("¡Pokémon no encontrado!")
+        return res.status(404).send("¡Pokémon no encontrado!")
     }
 
 
 })
 
-app.get("/pokemon/:name", (req, res, next)=>{
+app.get("/pokemon/:name([A-Za-z]+)", (req, res, next)=>{
     const name = req.params.name;
-    for(i =0; i < pokemon.length; i++){
-       if(pokemon[i].name == name){
-        res.status(200);
-        res.send(pokemon[i]);
-       } 
-    }
-    res.status(404);
-    res.send("Pokémon no encontrado")
+//For:
+    // for(i =0; i < pokemon.length; i++){
+    //    if(pokemon[i].name.toUpperCase() == name.toUpperCase()){
+    //     return res.status(200).send(pokemon[i]);
+    //    } 
+    // }
+//Filter: 
+    const pk = pokemon.filter((p)=>{
+        // condicion ? valor si verdadero : valor si falso
+        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+    })
+
+     return  (pk.length > 0) ? res.status(200).send(pk) :  res.status(404).send("Pokémon no encontrado")
+
+    // res.status(404).send("Pokémon no encontrado")
 })
 
 //Dejamos que el usuario ponga cualquier dato
