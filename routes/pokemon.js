12c2417c8597e2fdab916369aec13dfd7a56c077
Alan Alvarez <aran.712@gmail.com>
2022-03-14 17:28:56 -0600

    Added: Pokemon POST

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,15 +3,31 @@ const pokemon = express.Router();
 const pk = require("../pokedex.json").pokemon;
 const db = require("../config/database");
 
-pokemon.post("/", (req, res, next)=>{
-    return res.status(200).json(req.body)
+pokemon.post("/", async (req, res, next)=>{
+    const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+
+    if(pok_name && pok_height && pok_weight && pok_base_experience){
+        let query = "INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
+        query += `VALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+        const rows = await db.query(query);
+        console.log(rows);
+    
+        if(rows.affectedRows == 1){
+            return res.status(201).json({
+                code: 201,
+                message: "Pokemon insertado correctamente"
+            }); 
+        } 
+        return res.status(500).json({code: 500, message: "Ocurrió un error"})
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"})
 })
 
 pokemon.get("/", async (req,res,next)=>{
     const pkmn = await db.query("SELECT * FROM pokemon");
     return res.status(200).json(
         {
-            code: 1,
+            code: 200,
             message: pkmn
         }
     );
@@ -23,7 +39,7 @@ pokemon.get("/:id([0-9]{1,3})", async  (req,res,next)=>{
     const id = req.params.id;
     const pokemonId = await db.query(`SELECT * FROM pokemon WHERE pok_id = ${id}`);
     id >= 1 && id <= 722
-      ? res.status(200).json({code: 1, message: pokemonId})
+      ? res.status(200).json({code: 200, message: pokemonId})
       : res.status(404).json({code: 404, message: "Pokemon no encontrado"});
 })
 
@@ -32,7 +48,7 @@ pokemon.get("/:name([A-Za-z]+)", async  (req, res, next)=>{
   const pokemonName = await db.query(`SELECT * FROM pokemon WHERE pok_name = "${name}"`);
 
   pokemonName.length > 0
-    ? res.status(200).json({code: 1, message: pokemonName})
+    ? res.status(200).json({code: 200, message: pokemonName})
     : res.status(404).json({code: 404, message: "Pokemon no encontrado"});
 })
 module.exports = pokemon;
